618caa4aa7ab720d31717f242ea768367f974885
Vincent <vince.pno@gmail.com>
2025-12-30 11:35:06 +0100

    logger

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,4 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::broadcast;
+use dashmap::DashMap;   
+use std::fmt;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum WireMsg {
@@ -7,6 +11,19 @@ pub enum WireMsg {
 
     /// Publier sur le topic de cette connexion
     Publish { payload: Vec<u8> },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Topic {
+    Logs,
+}
+
+impl fmt::Display for Topic {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Topic::Logs => write!(f, "Logs"),
+        }
+    }
 }
 
 pub fn encode(msg: &WireMsg) -> Vec<u8> {
@@ -26,4 +43,42 @@ pub struct LookupResp {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RegisterResp {
     pub ok: bool,
+}
+
+#[derive(Clone)]
+pub struct NodeState {
+    master: String,
+    listen_addr: String, 
+    topics: Arc<DashMap<String, broadcast::Sender<Vec<u8>>>>,
+    http: reqwest::Client,
+}
+
+impl NodeState {
+    pub fn new(master: String, port: String) -> Self {
+        Self {
+            master,
+            listen_addr: format!("0.0.0.0:{}", port),
+            topics: Arc::new(DashMap::new()),
+            http: reqwest::Client::new(),
+        }
+    }
+}
+
+pub async fn open_topic(st: &NodeState, topic: Topic) -> anyhow::Result<()> {
+    // broadcast channel
+    let (tx, _rx) = broadcast::channel::<Vec<u8>>(1024);
+    st.topics.insert(topic.to_string(), tx);
+
+    // register auprès du master (GET)
+    let url = format!(
+        "{}/register?topic={}&addr={}",
+        st.master,
+        urlencoding::encode(topic.to_string().as_str()),
+        urlencoding::encode(&st.listen_addr)
+    );
+
+    let resp = st.http.get(url).send().await?.error_for_status()?;
+    let _json: serde_json::Value = resp.json().await?;
+    println!("[master] new topic '{topic}' at {}", st.listen_addr);
+    Ok(())
 }
--- crates/logger/src/main.rs
+++ crates/logger/src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
-    println!("logger V1");
-    //println!("{}", core::add(5, 7));
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long)]
+    master: String, // master ip and port ex: http://127.0.0.1:8080
+
+    #[arg(long)]
+    port: String, // listening port ex: 9001
+
+}
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("[logger] V1");
+    let args = Args::parse();
+    let st = core::NodeState::new(args.master, args.port);
+    core::open_topic(&st, core::Topic::Logs).await?;
+    println!("[logger] running. Press Ctrl+C to stop.");
+    tokio::signal::ctrl_c().await?;
+    Ok(())
 }
